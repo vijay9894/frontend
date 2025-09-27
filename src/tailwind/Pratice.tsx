22efa7e02f026435770c688f5e6e09a75e8716fa
vijay9894 <sanaboinavijaykrishna@gmail.com>
2025-09-27 21:52:58 +0530

    feat: Add Avatar component and integrate it into the Pratice component

--- src/tailwind/Pratice.tsx
+++ src/tailwind/Pratice.tsx
@@ -9,25 +9,46 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 function Pratice() {
-    return (
-        <div className="h-screen bg-black ">
-            <Card className="w-96 bg-white text-black">
-                <CardHeader>
-                    <CardTitle className="font-semibold">Bahubali Movie</CardTitle>
-                    <CardDescription>ACtor Prabhas</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p><Button className="cursor-pointer "> See Movie </Button></p>
-                </CardContent>
-                <CardFooter>
-                    <p>Telugu Film Indian Biggest BlockBuster </p>
-                </CardFooter>
-            </Card>
 
-        </div>
+
+    function handleClick() {
+        alert("Ticket Booked Successfully")
+    }
+
+    const movies = [
+        { name: "Bahubali", actor: "Prabhas", about: "Telugu Film Indian Biggest BlockBuster" }
+        , { name: "Bahubali", actor: "Prabhas", about: "Telugu Film Indian Biggest BlockBuster" },
+        { name: "Bahubali", actor: "Prabhas", about: "Telugu Film Indian Biggest BlockBuster" }
+    ]
+
+    return (
+        <>
+            <div className="h-20 bg-green-50 flex justify-end items-center">
+                <Avatar className="m-5 cursor-pointer" onClick={() => alert("Profile Clicked")}>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div className="h-170 bg-black flex">
+                {movies.map((item) => (
+                    <Card className="w-96 mx-2 my-2 pb-12 h-50  bg-white text-black flex">
+                        <CardHeader>
+                            <CardTitle className="font-semibold">{item.name}</CardTitle>
+                            <CardDescription>{item.actor}</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <p><Button className="cursor-pointer" onClick={handleClick}> Book Ticket </Button></p>
+                        </CardContent>
+                        <CardFooter>
+                            <p>{item.about}</p>
+                        </CardFooter>
+                    </Card>
+                ))}
+            </div>
+        </>
     )
 }
 
