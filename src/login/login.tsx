34a6864467e30e721eae6c0737cf92b4343e6223
vijaykrishna-railtelit <vijayk.sanaboina@gmail.com>
2025-09-30 17:04:44 +0530

    feat: update Google login button icon and improve styling in MyLogin component

--- src/login/login.tsx
+++ src/login/login.tsx
@@ -1,6 +1,6 @@
 import { useNavigate , useLocation } from "react-router-dom";
 import { useEffect } from "react";
-import { FaGoogle } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 function MyLogin() {
 
@@ -44,8 +44,8 @@ function MyLogin() {
       window.location.href = await "http://localhost:3000/google/login"
  }
 
-    return <div className="flex justify-center items-center h-screen bg-white">
-        <div className="border-2 border-pink text-white rounded-lg lg:w-105 lg:h-96 p-10 bg-gray-800 sm:h-full w-full flex flex-col justify-center">
+    return <div className="flex justify-center items-center h-screen bg-gray-700">
+        <div className="border-2 border-pink text-white rounded-lg lg:w-105 lg:h-96 p-10 bg-black sm:h-full w-full flex flex-col justify-center shadow-xl">
             <div className="flex justify-center font-semibold sm:font-bold">   Login Form </div>
             <div className="flex flex-col space-y-8 m-10 justify-center h-60">
                 <input type=" text" id="myInput" placeholder="Enter your email" className="rounded-lg p-2 text-white hover:bg-blue-20" />
@@ -53,8 +53,8 @@ function MyLogin() {
                 <button onClick={handleClick} className="bg-blue-600 text-white rounded-lg p-2 text-lg hover:bg-blue-400 ">Login </button>
             </div>
             <hr></hr>
-            <div className="flex justify-center">
-                <button onClick={handleEmailLogin} className=" flex text-white rounded-lg p-2 text-lg cursor-pointer hover:bg-gray-500 ">Continue with Google  <FaGoogle className=" ml-2 mt-1.5 "/>  </button>
+            <div className="flex justify-center mt-4">
+                <button onClick={handleEmailLogin} className=" flex text-white rounded-lg p-2 text-lg cursor-pointer">Continue with Google  <FcGoogle className=" text-3xl ml-2"/>  </button>
             </div>
         </div>
     </div>
