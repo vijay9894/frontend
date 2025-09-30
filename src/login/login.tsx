c5857110fff634d1c78a01c478d6ebdb818c5e9d
vijaykrishna-railtelit <vijayk.sanaboina@gmail.com>
2025-09-30 16:49:53 +0530

    feat: add Google login button to MyLogin component; update styling and layout

--- src/login/login.tsx
+++ src/login/login.tsx
@@ -1,5 +1,6 @@
 import { useNavigate , useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import { FaGoogle } from "react-icons/fa";
 
 function MyLogin() {
 
@@ -43,14 +44,17 @@ function MyLogin() {
       window.location.href = await "http://localhost:3000/google/login"
  }
 
-    return <div className="flex justify-center items-center h-screen bg-black">
-        <div className="border-2 border-pink text-white rounded-lg lg:w-96 lg:h-96 p-10 bg-gray-800 sm:h-full w-full flex flex-col justify-center">
-            <div className="flex justify-center font-semiboldc sm:font-bold">   Login Form </div>
+    return <div className="flex justify-center items-center h-screen bg-white">
+        <div className="border-2 border-pink text-white rounded-lg lg:w-105 lg:h-96 p-10 bg-gray-800 sm:h-full w-full flex flex-col justify-center">
+            <div className="flex justify-center font-semibold sm:font-bold">   Login Form </div>
             <div className="flex flex-col space-y-8 m-10 justify-center h-60">
                 <input type=" text" id="myInput" placeholder="Enter your email" className="rounded-lg p-2 text-white hover:bg-blue-20" />
                 <input type="password" id="myPassword" placeholder="Enter your password" className="rounded-lg p-2 text-white hover:bg-blue-20" />
                 <button onClick={handleClick} className="bg-blue-600 text-white rounded-lg p-2 text-lg hover:bg-blue-400 ">Login </button>
-                <button onClick={handleEmailLogin} className="bg-blue-600 text-white rounded-lg p-2 text-lg hover:bg-blue-400 ">Login With Email </button>
+            </div>
+            <hr></hr>
+            <div className="flex justify-center">
+                <button onClick={handleEmailLogin} className=" flex text-white rounded-lg p-2 text-lg cursor-pointer hover:bg-gray-500 ">Continue with Google  <FaGoogle className=" ml-2 mt-1.5 "/>  </button>
             </div>
         </div>
     </div>
